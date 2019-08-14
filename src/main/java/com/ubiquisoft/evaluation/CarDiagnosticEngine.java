7b5431eb4f157ea6ef43353896578d95d0e11a65
itsongit <54100011+itsongit@users.noreply.github.com>
2019-08-14 04:58:40 -0400

    Add files via upload
    Initial Commit from Local

--- src/main/java/com/ubiquisoft/evaluation/CarDiagnosticEngine.java
+++ src/main/java/com/ubiquisoft/evaluation/CarDiagnosticEngine.java
@@ -9,6 +9,12 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CarDiagnosticEngine {
 
@@ -40,6 +46,75 @@ public class CarDiagnosticEngine {
 		 */
 
 
+
+		System.out.println(
+				"============================================================="
+		);
+		System.out.println(
+				"Welcome to UbiquiSoft State of the Art Car Diagnostic Center"
+		);
+		System.out.println(
+				"============================================================="
+		);
+
+
+		System.out.println("Starting Diagnostic Process");
+
+		System.out.println(">Initiating Step 1 ... Checking if Car is all good...");
+		//Diagnostic Step 1:
+		boolean isCarValid = validateCar(car);
+
+		System.out.println(">Step 1 .. Complete");
+
+		System.out.println(">>Moving to Step 2 ... Checking for any missing parts...");
+		//Diagnostic Step 2:
+		boolean isCarMissingParts = (isCarValid) ? checkForMissingParts(car) : true;
+		System.out.println(">>Step 2 .. Complete");
+
+
+		//Diagnostic Step 3:
+
+		System.out.println(">>>Moving to Step 3 ... Checking if all parts are working as expected...");
+		boolean areAllPartsWorking = (!isCarMissingParts) ? checkForWorkingParts(car) : false;
+		System.out.println(">>>Step 3 .. Complete");
+
+
+		StringBuffer statusMessage = new StringBuffer();
+
+
+		statusMessage.append(LocalDate.now());
+		statusMessage.append(" ");
+		statusMessage.append(car.getYear());
+		statusMessage.append(" ");
+		statusMessage.append(car.getMake());
+		statusMessage.append(" ");
+		statusMessage.append(car.getModel());
+		statusMessage.append(" ");
+
+		if(isCarValid && !isCarMissingParts && areAllPartsWorking){
+			//All good, display message on console
+
+
+			statusMessage.append("was found to be free of any issues.");
+
+			statusMessage.append(LocalDate.now());
+
+			System.out.println(
+					statusMessage.toString()
+			);
+
+
+		}else{
+
+			statusMessage.append("needs further attention as the diagnostic system found some issues. ");
+			System.out.println(
+					statusMessage.toString()
+			);
+
+		}
+
+
+
 	}
 
 	private void printMissingPart(PartType partType, Integer count) {
@@ -54,6 +129,127 @@ public class CarDiagnosticEngine {
 		if (condition == null) throw new IllegalArgumentException("ConditionType must not be null");
 
 		System.out.println(String.format("Damaged Part Detected: %s - Condition: %s", partType, condition));
+	}
+
+	private void printMissingCarFields(String nameOfField) {
+
+		if (nameOfField == null) throw new IllegalArgumentException("nameOfField must not be null");
+		System.out.println("Missing Car data field Detected: " +  nameOfField);
+	}
+
+	/**
+	 * This method checks three data fields in the Car object and if one or more are missing it returns false otherwise
+	 * returns true
+	 * @param car - Car object
+	 * @return false if Car object is missing at least one of the data fields
+	 * @throws IllegalArgumentException - if the car object is null
+	 */
+	private boolean validateCar(Car car){
+		boolean isCarValid = false;
+		String make = car.getMake();
+		String year = car.getYear();
+		String model = car.getModel();
+
+		if(null != car){
+			if( null != make || null != model || null != year){
+				isCarValid = true;
+			}else if(null == make){
+				isCarValid = false;
+				printMissingCarFields("make");
+			}else if(null == year){
+				isCarValid = false;
+				printMissingCarFields("year");
+
+			}else if(null == model){
+				isCarValid = false;
+				printMissingCarFields("model");
+			}
+
+		}else{
+			throw new IllegalArgumentException("Car must not be null");
+		}
+
+		return isCarValid;
+
+	}
+
+	/**
+	 * checkForMissingParts encapsulates the Step 2 in diagnostic steps.
+	 * It checks for missing parts and if it finds any, calls printMissingPart method
+	 * @param car - car object to run the disgnostic on
+	 * @return true if car object is missing any parts, false if all required parts are present
+	 * @throws IllegalArgumentException - if the car object is null
+	 *
+	 */
+
+	private boolean checkForMissingParts(Car car){
+		Map<PartType,Integer> missingPartsMap = car.getMissingPartsMap();
+		boolean isCarMissingParts = false;
+
+		if (null == car){
+			throw new IllegalArgumentException("Car must not be null");
+		}
+
+		if(null != missingPartsMap){
+
+			if (missingPartsMap.isEmpty()){
+				//all part types are present including 4 tires
+			}else{
+				isCarMissingParts = true;
+				//missingPartsMap.
+				Set<Map.Entry<PartType, Integer>> entries = missingPartsMap.entrySet();
+
+				Stream<Map.Entry<PartType, Integer>> carParts = entries.stream();
+
+				carParts.forEach(
+						entry -> printMissingPart(entry.getKey(), entry.getValue())
+				);
+
+			}
+		}
+
+	return isCarMissingParts;
+	}
+
+
+	/**
+	 * Encapsulates Step 3 in the Diagnostic process
+	 * @param car
+	 * @return true if car object has all its parts working, false if any parts are not
+	 * @throws IllegalArgumentException - if the car object is null
+	 */
+	private boolean checkForWorkingParts(Car car){
+		List<Part> carParts = car.getParts();
+		List<Part> damagedPartsList = null;
+		boolean areAllPartsWorking = false;
+
+		if (null == car){
+			throw new IllegalArgumentException("Car must not be null");
+		}
+
+		if(null != carParts){
+
+			damagedPartsList =
+					carParts.
+							stream().
+							filter(
+									c -> !c.isInWorkingCondition()
+							).
+							collect(Collectors.toList());
+
+
+			damagedPartsList.
+					forEach(
+							damagedPart -> printDamagedPart(damagedPart.getType(),damagedPart.getCondition() )
+					);
+
+		}
+
+		int damagedPartCount = (null != damagedPartsList) ? damagedPartsList.size() : 0;
+
+		areAllPartsWorking = (damagedPartCount == 0) ? true : false;
+
+		return areAllPartsWorking;
 	}
 
 	public static void main(String[] args) throws JAXBException {
@@ -72,6 +268,14 @@ public class CarDiagnosticEngine {
 		Unmarshaller unmarshaller = context.createUnmarshaller();
 
 		Car car = (Car) unmarshaller.unmarshal(xml);
+
+		/*
+		System.out.println("Car is " + car.getMake());
+
+		List<Part> parts = car.getParts();
+
+		parts.stream().forEach(part -> System.out.println(part.toString()));
+		*/
 
 		// Build new Diagnostics Engine and execute on deserialized car object.
 
--- src/main/java/com/ubiquisoft/evaluation/domain/Car.java
+++ src/main/java/com/ubiquisoft/evaluation/domain/Car.java
@@ -3,8 +3,12 @@ package com.ubiquisoft.evaluation.domain;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -16,6 +20,17 @@ public class Car {
 
 	private List<Part> parts;
 
+	/**
+	 * It calculates an internal maps of how many part types are present in the car object and their corresponding count.
+	 * It compares it with a reference model to see which ones of part types from the reference model are missing from
+	 * the actual part types found in the car object.
+	 *
+	 * Assumptions made: the returned Map<PartType, Integer> contains an object only is it's missing and corresponding
+	 * count of missing parts. For example, if car is missing an Engine, 3 tires and and nothing else,
+	 * the returned Map will contains PartType.ENGINE, count 1, PartType.TIRE and count 3
+	 *
+	 * @return Map <PartType,Integer> - map of missing parts - keys are part types and values are number of those part types
+	 */
 	public Map<PartType, Integer> getMissingPartsMap() {
 		/*
 		 * Return map of the part types missing.
@@ -31,7 +46,56 @@ public class Car {
 		 *      }
 		 */
 
-		return null;
+		Map<PartType, Integer> missingPartType = new HashMap<>();
+
+		if(null != parts){
+
+
+			Hashtable<PartType, Integer> referenceModel = setupWorkingCarReference();
+
+
+			Map<PartType, Integer> frequencyMap = new HashMap<>();
+			for (Part part: parts) {
+				PartType partType = part.getType();
+				Integer count = frequencyMap.get(partType);
+				if (count == null)
+					count = 0;
+
+				frequencyMap.put(partType, count + 1);
+			}
+
+
+			for (Map.Entry<PartType, Integer> entry : referenceModel.entrySet()) {
+
+				PartType partType = entry.getKey();
+				Integer modelCount = entry.getValue();
+
+
+				boolean actualPartTypeExists = frequencyMap.containsKey(partType);
+
+				if(actualPartTypeExists){
+					//The part type exists for car object
+					Integer actualCount  = frequencyMap.get(partType);
+					//if the number of part type is same as what should be in the model car object, it's not missing,
+					// it doesn't need to be stored in Map that will be eventually returned from this method
+					if(modelCount == actualCount){
+						//can be removed later, leaving it in there as it has accompanying comments for clarity
+					}else if(modelCount - actualCount > 0){
+						missingPartType.put(partType, modelCount - actualCount);
+
+					}
+				}else{ // this particular part type was not found in the actual car object, so it's missing and the
+					// count can be whatever the model contains
+					missingPartType.put(partType, modelCount);
+				}
+
+			}
+
+		}
+
+
+
+		return missingPartType;
 	}
 
 	@Override
@@ -84,4 +148,20 @@ public class Car {
 	/*  Getters and Setters End *///endregion
 	/* --------------------------------------------------------------------------------------------------------------- */
 
+	/**
+	 * Setting up a reference model of working car with the required parts and their part counts
+	 * @return
+	 */
+	private Hashtable<PartType,Integer> setupWorkingCarReference() {
+		Hashtable<PartType, Integer> workingModel = new Hashtable<PartType,Integer>();
+
+		workingModel.put(PartType.ENGINE, new Integer(1));
+		workingModel.put(PartType.ELECTRICAL, new Integer(1));
+		workingModel.put(PartType.FUEL_FILTER, new Integer(1));
+		workingModel.put(PartType.OIL_FILTER, new Integer(1));
+		workingModel.put(PartType.TIRE, new Integer(4));
+
+		return workingModel;
+
+	}
 }
